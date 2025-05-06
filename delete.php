acfa5f48aa8192e1cd4d0c1bace99426b2fab0f0
Camila Rocio Serafini <camiiserafinii@gmail.com>
2025-05-06 16:58:46 -0300

    first commit

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM students WHERE id = $id";
+
+echo "<link rel='stylesheet' href='style.css?v=" . time() . "'>";
+
+if ($connection->query($sql) === TRUE) {
+    header("Location: index.php");
+    exit;
+} else {
+    echo "Error al borrar: " . $connection->error;
+}
+?>
